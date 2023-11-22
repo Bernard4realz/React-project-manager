c30663a0b7783387a7c212ec43422170bd500ba9
bernard4realz <bernardnwakor@gmail.com>
2023-11-23 00:00:41 +0100

    worked on the input and new project section

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,6 +1,6 @@
 export default function({label,textarea, ...props}){
-    return <p>
-        <label>{label}</label>
+    return <p className="flex flex-col gap-1 my-2">
+        <label className="text-[#776B5D] text-sm font-bold uppercase">{label}</label>
         {textarea? <textarea {...props}></textarea> : <input {...props}></input>}
-    </p>
+    </p> 
 }
--- src/Components/NewProject.jsx
+++ src/Components/NewProject.jsx
@@ -3,24 +3,26 @@ import Input from "./Input";
 export default function NewProject() {
   return (
     <>
-      <div>
-        <Input label={"Title"} />
-        <Input label={"Description"} textarea />
-        <Input label={"Due Date"} props={"type=date"} />
-      </div>
-      <div>
-        <menu>
-          <li>
-            <button className="mx-2 my-1 px-2 text-white bg-slate-700">
-              Clear
-            </button>
-          </li>
-          <li>
-            <button className="mx-2 my-1 px-2 text-white bg-slate-700">
-              Create
-            </button>
-          </li>
-        </menu>
+      <div className="w-[35rem] mt-16">
+        <div >
+          <Input label={"Title"} />
+          <Input label={"Description"} textarea />
+          <Input label={"Due Date"} props={"type=date"} />
+        </div>
+        <div>
+          <menu className="flex items-center justify-end my-4">
+            <li>
+              <button className="mx-2 my-1 px-2 text-white bg-[#776B5D] hover:bg-black rounded-md">
+                Clear
+              </button>
+            </li>
+            <li>
+              <button className="mx-2 my-1 px-2 text-white bg-[#776B5D] hover:bg-black rounded-md">
+                Create
+              </button>
+            </li>
+          </menu>
+        </div>
       </div>
     </>
   );
