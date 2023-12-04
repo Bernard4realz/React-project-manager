33ec25853170fc8ed4285060bc711e7c34b9f2ab
bernard4realz <bernardnwakor@gmail.com>
2023-12-04 13:19:36 +0100

    Worked on the modal and also the clear button

--- src/App.jsx
+++ src/App.jsx
@@ -3,52 +3,61 @@ import NewProject from "./Components/NewProject";
 import NoProjectSelected from "./Components/NoProjectSelected";
 import SideBar from "./Components/SideBar";
 
-
 function App() {
-const [projectsState, setProjectsState]= useState({
-  projectSelectedId: undefined, 
-  projects: []
-})
+  const [projectsState, setProjectsState] = useState({
+    projectSelectedId: undefined,
+    projects: [],
+  });
 
-function handleStartAdd(){
-  setProjectsState(prevProjects =>{
-    return{
-      ...prevProjects,
-      projectSelectedId: null
-    }
-  })
-}
+  function handleStartAdd() {
+    setProjectsState((prevProjects) => {
+      return {
+        ...prevProjects,
+        projectSelectedId: null,
+      };
+    });
+  }
+  function handleCancelAdd(){
+    setProjectsState((prevProjects) => {
+      return {
+        ...prevProjects,
+        projectSelectedId: undefined,
+      };
+    });
+  }
 
-let content; 
+  let content;
 
-if(projectsState.projectSelectedId === undefined){
-  content = <NoProjectSelected onStartAddProject={handleStartAdd}/>
-}else if(projectsState.projectSelectedId === null){
-  content = <NewProject onAdd={handleAddProject}/>
-}
+  if (projectsState.projectSelectedId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAdd} />;
+  } else if (projectsState.projectSelectedId === null) {
+    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAdd} />;
+  }
 
-function handleAddProject(projectData){
-  setProjectsState(prevProjects =>{
-    const projectId = Math.random()
-    const newProject = {
-      ...projectData, 
-      Id: projectId
-    }
-    return{
-      ...prevProjects,
-      projectSelectedId: undefined,
-      projects: [prevProjects.projects, newProject]
-    }
-  })
-  
-}
+  function handleAddProject(projectData) {
+    setProjectsState((prevProjects) => {
+      const projectId = Math.random();
+      const newProject = {
+        ...projectData,
+        Id: projectId,
+      };
+      return {
+        ...prevProjects,
+        projectSelectedId: undefined,
+        projects: [prevProjects.projects, newProject],
+      };
+    });
+  }
   return (
     <>
       <div className="h-screen my-8 flex gap-8">
-        <SideBar projects={projectsState.projects} onStartAddProject={handleStartAdd}/>
+        <SideBar
+          projects={projectsState.projects}
+          onStartAddProject={handleStartAdd}
+        />
         {content}
       </div>
-    </> 
+    </>
   );
 }
 
--- src/Components/NewProject.jsx
+++ src/Components/NewProject.jsx
@@ -1,7 +1,9 @@
 import { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
 
-export default function NewProject({ onAdd }) {
+export default function NewProject({ onAdd, onCancel }) {
+  const modal = useRef();
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -11,7 +13,14 @@ export default function NewProject({ onAdd }) {
     const enteredDescription = description.current.value;
     const enteredDueDate = dueDate.current.value;
 
-    //validation
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      modal.current.open();
+      return;
+    }
 
     onAdd({
       title: enteredTitle,
@@ -22,21 +31,33 @@ export default function NewProject({ onAdd }) {
 
   return (
     <>
+      <Modal ref={modal} buttonCaption={"Close"}>
+        <h2 className='text-xl font-bold text-stone-600 my-4'>Invalid Input</h2>
+        <p className='text-stone-600 mb-4'>ooops... it's like you forgot to add a value</p>
+        <p className='text-stone-600 mb-4'>Please input values for every input field.</p>
+      </Modal>
       <div className="w-[35rem] mt-16">
         <div>
-          <Input type= 'text' ref={title} label={"Title"} />
+          <Input type="text" ref={title} label={"Title"} />
           <Input ref={description} label={"Description"} textarea />
-          <Input type= 'date' ref={dueDate} label={"Due Date"} props={"type=date"} />
+          <Input
+            type="date"
+            ref={dueDate}
+            label={"Due Date"}
+          />
         </div>
         <div>
           <menu className="flex items-center justify-end my-4">
             <li>
-              <button className="mx-2 my-1 px-2 text-white bg-[#776B5D] hover:bg-black rounded-md">
+              <button className="mx-2 my-1 px-2 text-white bg-[#776B5D] hover:bg-black rounded-md" onClick={onCancel}>
                 Clear
               </button>
             </li>
             <li>
-              <button className="mx-2 my-1 px-2 text-white bg-[#776B5D] hover:bg-black rounded-md" onClick={handleSave}>
+              <button
+                className="mx-2 my-1 px-2 text-white bg-[#776B5D] hover:bg-black rounded-md"
+                onClick={handleSave}
+              >
                 Create
               </button>
             </li>
